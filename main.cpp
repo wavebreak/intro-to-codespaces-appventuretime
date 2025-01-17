8c91976819275904da49c3a414f1ef57d317806e
appventuretime <will@appventuretime.com>
2025-01-17 22:28:10 +0000

    Demoed Debugging

--- main.cpp
+++ main.cpp
@@ -2,6 +2,10 @@
 using namespace std;
 
 int main() {
+    int age;
     cout << "Hello, Codespaces! This is user AppVentureTime!" << endl;
+    cout << "Enter your age: "<<endl;
+    cin >> age;
+    cout << "You are " << age << " years old."<<endl;
     return 0;
 }
